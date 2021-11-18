1f81506f434c251866c68b483c61e59379620ac7
shah-zx <shahnawazsayyed456@gmail.com>
2021-11-18 22:53:35 +0530

    Done

---
+++ .history/mini-project-1_20211118225212.py
@@ -0,0 +1,21 @@
+# Here we will make a mini project :
+
+# Requirements :
+# method for : Display books
+# method for : lend book
+# method for : add book
+# method for : return book
+
+class Library:
+    def __init__(self, listofbooks, library_name):
+        self.alistofbooks = listofbooks
+        self.alibrary_name = library_name
+
+    def info(alibrary_name, alistofbooks):
+        print(f"The name of the library is : {alibrary_name} and these are the number of books : {alistofbooks}")
+
+
+Nl = Library("eight thousand" , "National Library")
+print(Nl.alistofbooks)
+print(Nl.alibrary_name)
+
